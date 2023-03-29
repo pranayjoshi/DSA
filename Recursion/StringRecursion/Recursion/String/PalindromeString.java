a8cd4f1850c98b4eee2680350099e077e01f0633
pranayjoshi <pranayjoshi4466@gmail.com>
2023-03-29 20:30:31 +0530

    String recursion

---
+++ Recursion/StringRecursion/Recursion/String/PalindromeString.java
@@ -0,0 +1,20 @@
+package Recursion.StringRecursion.Recursion.String;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+        String str = "naman";
+        boolean x = isPalindrome(str, 0, 0);
+        System.out.println(x);
+    }
+
+    public static boolean isPalindrome(String str, int l, int r){
+
+        if (l >= r) return true;
+
+        if (str.charAt(l) != str.charAt(r)){
+            return false;
+        }
+
+        return isPalindrome(str, (l+1), (r+1));
+    }
+}
