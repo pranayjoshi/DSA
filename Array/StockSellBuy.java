d43603dcaed90f0314f0f743842fbdd2644bbdbe
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-03 12:26:29 +0530

    added

--- Array/StockSellBuy.java
+++ Array/StockSellBuy.java
@@ -8,7 +8,25 @@ public class StockSellBuy {
 
     public static void UsingSpace(int[] a){
         int[] spacedArray = new int[ a.length] ;
-        for (int i = 0; i < a.length; i++)
-            System.out.println(spacedArray[i]);
+        spacedArray[spacedArray.length-1] = a[a.length-1];
+
+        int buy = 0;
+
+        for (int i = a.length-2; i >= 0; i--){
+            if (a[i] > a[i+1]){
+                spacedArray[i] = a[i];
+            } else {
+                spacedArray[i] = a[i+1];
+            }
+
+            if ((spacedArray[i] - a[i]) > buy) buy = a[i];
+        }
+
+        int sell = spacedArray[0];
+
+        System.out.println(spacedArray);
+        System.out.println(buy);
+        System.out.println(sell);
+            
     }
 }
