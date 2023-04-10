f8afe86decebe4e73c4f859e7f4c52cfe9bddc43
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-10 19:46:05 +0530

    added

--- HashMap/HashMap.java
+++ HashMap/HashMap.java
@@ -3,7 +3,7 @@ package HashMap;
 public class HashMap {
     public static int[] A = {3,1,2,4,0,1,3,2};
     public static void main(String[] args) {
-        Insert();
+        // Insert();
     }
 
     static void Insert(int n,int value){
@@ -18,6 +18,22 @@ public class HashMap {
             } else return;
         }
     }
+
+    static void Delete(int n){
+        A[1] = A[n];
+        n = n-1;
+        int i = 1;
+        while ( i < n){
+            int left = A[2*i];
+            int right = A[(2*i)+1];
+            int main = left > right ? left : right;
+            if (A[main] > A[i]){
+                swap(i, main);
+                i = main;
+            } else return;
+        }
+    }
+
     static void swap(int i, int j){
         int temp = A[i];
         A[i] = A[j];
