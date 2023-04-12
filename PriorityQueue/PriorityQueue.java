5a93bd0e8d42f87a512ec9571094412ffbfd76fa
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-12 17:43:01 +0530

    added

--- PriorityQueue/PriorityQueue.java
+++ PriorityQueue/PriorityQueue.java
@@ -1,7 +1,7 @@
 package PriorityQueue;
 
-import java.util.Collection;
-import java.util.Collections;
+// import java.util.Collection;
+// import java.util.Collections;
 
 public class PriorityQueue {
     public static void main(String[] args) {
