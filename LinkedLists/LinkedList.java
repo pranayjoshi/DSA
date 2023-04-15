900c1679a1af819727b8e96ea1c1290611e7f394
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-16 01:55:17 +0530

    added

--- LinkedLists/LinkedList.java
+++ LinkedLists/LinkedList.java
@@ -18,6 +18,26 @@ public class LinkedList {
         }
     }
 
+    static void Insert(int data, Node head, int pos){
+        Node toAdd = new Node(data);
+
+        if (pos == 0){
+            toAdd.next = head;
+            head = toAdd;
+            return;
+        }
+
+        Node prev = head;
+
+        for ( int i = 0; i < pos-1; i++){
+            prev = prev.next;
+        }
+
+        toAdd.next = prev.next;
+        prev.next = toAdd;
+
+    }
+
     public static void main(String[] args) {
         Node n1 = new Node(10);
         Node n2 = new Node(20);
