9a6c9632da32667039d725ef2ffb25ed8987923c
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-24 18:17:04 +0530

    added

--- Stack/Stacks.java
+++ Stack/Stacks.java
@@ -4,6 +4,15 @@ import java.util.Stack;
 public class Stacks {
     public static void main(String[] args) {
         Stack<Integer> s = new Stack<>();
+        s.push(10);
+        s.push(20);
+        s.push(30);
         
+        while (s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+
+        System.out.println(s.size());
     }
 }
