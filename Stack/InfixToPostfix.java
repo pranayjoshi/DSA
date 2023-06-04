107d407bdd5dc589d818303a97adbfb3cddcb859
pranayjoshi <pranayjoshi4466@gmail.com>
2023-06-04 08:31:23 +0530

    added:

--- Stack/InfixToPostfix.java
+++ Stack/InfixToPostfix.java
@@ -1,5 +1,18 @@
 package Stack;
 
+import java.util.Stack;
+
 public class InfixToPostfix {
+
+    public static String infix = "a + (b + c) / d";
+
+    public static Stack<Integer> s = new Stack<>();
     
+    public static void main(String[] args){
+
+    }
+
+    static void isHigherPrecedence(){
+        
+    }
 }
