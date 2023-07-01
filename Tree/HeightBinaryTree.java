2c34e9b5b0cf05f63092b39723a226aab75d6e00
pranayjoshi <pranayjoshi4466@gmail.com>
2023-07-01 10:36:11 +0530

    tree order

---
+++ Tree/HeightBinaryTree.java
@@ -0,0 +1,50 @@
+package Tree;
+
+import java.util.Scanner;
+
+public class HeightBinaryTree {
+	
+	static Scanner sc = null;
+    // static int counter = 0;
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		
+		Node root = createTree();
+        int s = HeightTree(root);
+		System.out.println(s);
+	}
+	
+	static Node createTree() {
+		
+		Node root = null;
+		System.out.println("Enter data: ");
+		int data = sc.nextInt();
+		
+		if(data == -1) return null;
+		
+		root = new Node(data);
+		
+		System.out.println("Enter left for " + data);
+		root.left = createTree();
+		
+		System.out.println("Enter right for "+ data);
+		root.right = createTree();
+		
+		return root;
+	}
+
+    static int HeightTree(Node root){
+        if (root == null) return 0;
+        return Math.max(HeightTree(root.left), HeightTree(root.right))+1;
+
+    }
+}
+
+class Node {
+	Node left, right;
+	int data;
+	
+	public Node(int data) {
+		this.data = data;
+	}
+}
