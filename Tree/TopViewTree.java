bf9bfc02cfa30c1c7fa84e385aef6f8ac6d4780d
pranayjoshi <pranayjoshi4466@gmail.com>
2023-07-02 21:41:36 +0530

    added

---
+++ Tree/TopViewTree.java
@@ -0,0 +1,44 @@
+package Tree;
+
+import java.util.Scanner;
+
+// import Node;
+
+public class TopViewTree {
+	
+	static Scanner sc = null;
+    // static int counter = 0;
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		
+		createTree();
+	}
+	
+	static Node createTree() {
+		
+		Node root = null;
+		System.out.println("Enter data: ");
+		int data = sc.nextInt();
+		
+		if(data == -1) return null;
+		
+		root = new Node(data);
+		
+		System.out.println("Enter left for " + data);
+		root.left = createTree();
+		
+		System.out.println("Enter right for "+ data);
+		root.right = createTree();
+		
+		return root;
+	}
+}
+
+class Node {
+	Node left, right;
+	int data;
+	
+	public Node(int data) {
+		this.data = data;
+	}
+}
