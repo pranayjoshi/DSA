6960422de444cd51816a062d16523a92df61aba7
pranayjoshi <pranayjoshi4466@gmail.com>
2023-07-23 20:38:41 +0530

    added

--- BST/Delete.java
+++ BST/Delete.java
@@ -4,20 +4,36 @@ public class Delete {
 
     public static void main(String[] args) {
         Node x = new Node(10);
-        System.out.println(insert(x, 10));
+        System.out.println(delete(x, 10));
     }
 
-    static Node insert(Node root, int Key) {
+    static int minValue(Node root) {
+        int minv = root.data;
+        while (root.left == null){
+            minv = root.left.data;
+            root = root.left;
+        }
+        return minv;
+    }
+
+    static Node delete(Node root, int Key) {
         // your code here
         if(root==null){
-            Node n= null;
-            return n;
+            return root;
         }
         if(root.data>Key){
-            root.left=insert(root.left,Key);
+            root.left=delete(root.left,Key);
         }
         else if(root.data<Key){
-            root.right=insert(root.right,Key);
+            root.right=delete(root.right,Key);
+        }
+
+        else {
+            if (root.left == null) return root.right;
+            else if (root.right == null) return root.left;
+            root.data = minValue(root.right);
+
+            root.right = delete(root.right, data)
         }
         return root;
     }
