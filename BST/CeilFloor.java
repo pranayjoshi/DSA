178ef4f10d060a2470815c606fef0f4223bd5ee5
pranayjoshi <pranayjoshi4466@gmail.com>
2023-07-30 06:38:47 +0530

    added

--- BST/CeilFloor.java
+++ BST/CeilFloor.java
@@ -1,7 +1,29 @@
 package BST;
 
 public class CeilFloor {
-    
+    public static void main(String[] args) {
+        Node x = new Node(10);
+        System.out.println(Ceil(x, 10));
+        System.out.println(Floor(x, 10));
+    }
+
+    static Integer Ceil(Node root, int Key) {
+        // your code here
+        int ans = Integer.MAX_VALUE;
+        if (root.data == Key) {
+            return root.data;
+        }
+        while (root != null) {
+
+            if (root.data > Key) {
+                root = root.left;
+            } else {
+                ans = root.data;
+                root = root.right;
+            }
+        }
+        return ans;
+    }
 }
 
 class Node {
